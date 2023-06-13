a2d4c435f00fd69c4d683b837dc2c4d746c21a28
Abhishek Magdum <abhishek.magdum.07@gmail.com>
2023-06-13 10:00:38 +0530

    Added : Save the greeting messages in the repository

--- src/main/java/com/bridgelabz/greeting/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greeting/controller/GreetingController.java
@@ -2,21 +2,25 @@ package com.bridgelabz.greeting.controller;
 
 import com.bridgelabz.greeting.entity.Greeting;
 import com.bridgelabz.greeting.entity.User;
+import com.bridgelabz.greeting.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private GreetingService greetingService;
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestBody User user){
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template,user.getFirstName()+" "+user.getLastName()));
+    public Greeting greeting(@RequestParam(defaultValue = "World") String firstName, String lastName) {
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return greetingService.addGreeting(user);
     }
-
 }
+
